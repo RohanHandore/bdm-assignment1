b7a9e8866d056d73be8694d7fddac8e8e9f86594
Rohanhandore <rohanhandore021@gmail.com>
2024-11-04 22:58:14 +0530

    mapper 4

--- mapper_q4.py
+++ mapper_q4.py
@@ -5,8 +5,8 @@ import sys
 for line in sys.stdin:
     parts = line.strip().split(',')
     if len(parts) > 3:  # Ensure there are enough columns
-        vehicle_type = parts[3]  # Vehicle type
-        location = parts[2]  # Assuming this is the location
+        vehicle_type = parts[14]  # Vehicle type
+        location = parts[12]  # Assuming this is the location
 
-        if vehicle_type in ['RIGID', 'ART']:  # Filter for HGV types
+        if vehicle_type in ['RIGID', 'HGV_ART']:  # Filter for HGV types
             print(f"{location}\t1")  # Emit location and count
